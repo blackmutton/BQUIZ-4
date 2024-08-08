5fb46b3ce1d9acc05249d8ff70d3abfd3cb89475
yt Chen <yutongc30@gmail.com>
2024-08-08 13:55:00 +0800

    登入成功時增加session做紀錄

--- api/chk_pw.php
+++ api/chk_pw.php
@@ -4,6 +4,13 @@ include "base.php";
 $table = $_GET['table'];
 unset($_GET['table']);
 
-echo $$table->count($_GET);
+if ($$table->count($_GET)) {
+    echo 1;
+    // $table會是Admin或Mem
+    $_SESSION[$table] = $_GET['acc'];
+} else {
+    echo 0;
+}
+// echo $$table->count($_GET);
 // echo $chk = $Mem->count($_GET);
 // $chk是0或>0，所以不需要再寫判斷式即可判斷帳號是否已存在
