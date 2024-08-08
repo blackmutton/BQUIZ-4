64da9c1e902009ab07a4de8ddc11fc72a6a5f644
yt Chen <yutongc30@gmail.com>
2024-08-08 16:14:24 +0800

    更新資料庫

--- sql/dbno4.sql
+++ sql/dbno4.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2024-08-05 10:11:00
+-- 產生時間： 2024-08-08 10:12:13
 -- 伺服器版本： 10.4.32-MariaDB
--- PHP 版本： 8.2.12
+-- PHP 版本： 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `dbno4`
+-- 資料庫： `db08`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) NOT NULL,
   `acc` text NOT NULL,
   `pw` text NOT NULL,
   `pr` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`, `pr`) VALUES
+(2, 'root', '5678', 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"5\";}'),
+(3, 'admin', '1234', 'a:5:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";}');
 
 -- --------------------------------------------------------
 
@@ -43,14 +51,14 @@ CREATE TABLE `admin` (
 CREATE TABLE `bottom` (
   `id` int(10) UNSIGNED NOT NULL,
   `bottom` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 傾印資料表的資料 `bottom`
 --
 
 INSERT INTO `bottom` (`id`, `bottom`) VALUES
-(1, '頁尾版權');
+(1, '2024-泰山訓練場版權所有');
 
 -- --------------------------------------------------------
 
@@ -70,7 +78,7 @@ CREATE TABLE `goods` (
   `big` int(10) UNSIGNED NOT NULL,
   `mid` int(10) UNSIGNED NOT NULL,
   `sh` int(1) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -87,7 +95,15 @@ CREATE TABLE `members` (
   `addr` text NOT NULL,
   `email` text NOT NULL,
   `regdate` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `members`
+--
+
+INSERT INTO `members` (`id`, `acc`, `pw`, `name`, `tel`, `addr`, `email`, `regdate`) VALUES
+(2, 'mem01', 'mem01', 'afdafsdf', '131231231', 'fasdfa', 'safdsfasd', '2024-08-08'),
+(4, 'mem02', '1234', 'mem02', '12312313', 'dsasdfasf', 'fdsafdfasfd', '2024-08-08');
 
 -- --------------------------------------------------------
 
@@ -106,7 +122,7 @@ CREATE TABLE `orders` (
   `total` int(10) UNSIGNED NOT NULL,
   `cart` text NOT NULL,
   `orderdate` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -118,7 +134,7 @@ CREATE TABLE `types` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` text NOT NULL,
   `big_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 已傾印資料表的索引
@@ -149,12 +165,6 @@ ALTER TABLE `members`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
-
---
 -- 資料表索引 `types`
 --
 ALTER TABLE `types`
@@ -168,7 +178,7 @@ ALTER TABLE `types`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
@@ -186,13 +196,7 @@ ALTER TABLE `goods`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- 使用資料表自動遞增(AUTO_INCREMENT) `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `types`
