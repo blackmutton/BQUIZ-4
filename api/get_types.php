f69e8a093a3cadede3448dfe14f399cedef686c9
yt Chen <yutongc30@gmail.com>
2024-08-09 14:44:53 +0800

    建立新增/修改商品頁面

--- api/get_types.php
+++ api/get_types.php
@@ -1,6 +1,6 @@
 <?php
 include "base.php";
-$row = $Type->all(['big_id' => $_POST['id']]);
+$row = $Type->all(['big_id' => $_GET['id']]);
 
 foreach ($rows as $row) {
     echo "<option value='{$row['id']}'{$row['name']}></option>";
--- js/js.js
+++ js/js.js
@@ -1,11 +1,62 @@
 // JavaScript Document
-function lof(x)
-{
-	location.href=x
+function lof(x) {
+	location.href = x
 }
 
-function del(table,id){
-	$.post("./api/del.php",{table,id},function(){
+function del(table, id) {
+	$.post("./api/del.php", { table, id }, function () {
 		location.reload()
-	}	)
+	})
+}
+function editType(id, dom) {
+	let name = $(dom).parent().prev().text();
+	let result = prompt("請輸入要修改的分類名稱", name);
+	if (result != null) {
+		$.post('api/edit_type.php', {
+			id, name: result
+		}, () => {
+			location.reload();
+			// $(dom).parent().prev().text(result);
+		})
+	}
+}
+function getTypes(type = 'big', id = 0) {
+	$.get("./api/get_types.php", {
+		type,
+		id
+	}, (types) => {
+		switch (type) {
+			case 'big':
+				$("#bigSelect").html(types)
+				break;
+			case 'mid':
+				$("#midSelect").html(types)
+				break;
+		}
+	})
+}
+
+function addType(type) {
+	let big, mid
+	switch (type) {
+		case "big":
+			big = $("#big").val()
+			$.post("api/add_type.php", {
+				type,
+				big
+			}, () => {
+				location.reload()
+			})
+			break;
+
+		case 'mid':
+			mid = $("#mid").val()
+			$.post("api/add_type.php", {
+				type,
+				big
+			}, () => {
+				location.reload()
+			})
+			break;
+	}
 }
