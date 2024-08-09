a5c65e16036952ca072c5d4c8c0d85f47f630ce2
yt Chen <yutongc30@gmail.com>
2024-08-09 08:55:41 +0800

    後台管理選單依登入管理者的權限來顯示

--- admin.php
+++ admin.php
@@ -1,5 +1,26 @@
 <?php
 include "./api/base.php";
+$login = $Admin->find(['acc' => $_SESSION['Admin']]);
+$loginPr = unserialize($login['pr']);
+
+// dd($login);
+// Array
+// (
+// [id] => 3
+// [acc] => admin
+// [pw] => 1234
+// [pr] => a:5:{i:0;s:1:"1";i:1;s:1:"2";i:2;s:1:"3";i:3;s:1:"4";i:4;s:1:"5";}
+// )
+// dd($loginPr);
+// Array
+// (
+// [0] => 1
+// [1] => 2
+// [2] => 3
+// [3] => 4
+// [4] => 5
+// )
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
@@ -25,12 +46,12 @@ include "./api/base.php";
 		</div>
 		<div id="left" class="ct">
 			<div style="min-height:400px;">
-				<a href="?do=admin">管理權限設置</a>
-				<a href="?do=th">商品分類與管理</a>
-				<a href="?do=order">訂單管理</a>
-				<a href="?do=mem">會員管理</a>
-				<a href="?do=bot">頁尾版權管理</a>
-				<a href="?do=news">最新消息管理</a>
+				<a href='?do=admin'>管理權限設置</a>
+				<?= (in_array(1, $loginPr)) ? "<a href='?do=th'>商品分類與管理</a>" : "" ?>
+				<?= (in_array(2, $loginPr)) ? "<a href='?do=order'>訂單管理</a>" : "" ?>
+				<?= (in_array(3, $loginPr)) ? "<a href='?do=mem'>會員管理</a>" : "" ?>
+				<?= (in_array(4, $loginPr)) ? "<a href='?do=bot'>頁尾版權管理</a>" : "" ?>
+				<?= (in_array(5, $loginPr)) ? "<a href='?do=news'>最新消息管理</a>" : "" ?>
 				<a href="?do=logout" style="color:#f00;">登出</a>
 			</div>
 		</div>
@@ -47,7 +68,8 @@ include "./api/base.php";
 		</div>
 		<div id="bottom" style="line-height:70px; color:#FFF; background:url(icon/bot.png);" class="ct">
 			<!-- 從backend/bot.php複製來的 -->
-		<?= $Bottom->find(1)['bottom'] ?> </div>
+			<?= $Bottom->find(1)['bottom'] ?>
+		</div>
 	</div>
 
 </body>
