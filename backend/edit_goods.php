b68d92ffe69202f9a9f5a74d6bceed17ea0b6a4c
yt Chen <yutongc30@gmail.com>
2024-08-12 09:27:54 +0800

    完成後台修改商品功能

--- backend/edit_goods.php
+++ backend/edit_goods.php
@@ -1,48 +1,54 @@
 <!-- 從add_goods.php複製而來 -->
-<h2 class="ct">修改商品</h2>
-<table class="all">
-    <tr>
-        <td class="tt ct">所屬大分類</td>
-        <td class="pp"><select name="big" id="bigSelect"></select></td>
-    </tr>
-    <tr>
-        <td class="tt ct">所屬中分類</td>
-        <td class="pp"><select name="mid" id="midSelect"></select></td>
-    </tr>
-    <tr>
-        <td class="tt ct">商品編號</td>
-        <td class="pp">完成分類後自動分配</td>
-    </tr>
-    <tr>
-        <td class="tt ct">商品名稱</td>
-        <td class="pp"><input type="text" name="name" id="name"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">商品價格</td>
-        <td class="pp"><input type="text" name="price" id="price"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">規格</td>
-        <td class="pp"><input type="text" name="spec" id="spec"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">庫存量</td>
-        <td class="pp"><input type="text" name="stock" id="stock"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">商品圖片</td>
-        <td class="pp"><input type="text" name="img" id="img"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">商品介紹</td>
-        <td class="pp"><input type="text" name="intro" id="intro"></td>
-    </tr>
-</table>
-<div class="ct">
-    <input type="submit" value="修改">
-    <input type="reset" value="重置">
-    <input type="button" value="返回">
-</div>
+<form action="./api/save_goods.php" method="post" enctype="multipart/form-data">
+    <table class="all">
+        <tr>
+            <td class="tt ct">所屬大分類</td>
+            <td class="pp">
+                <select name="big" id="bigSelect"></select>
+            </td>
+        </tr>
+        <tr>
+            <td class="tt ct">所屬中分類</td>
+            <td class="pp">
+                <select name="mid" id="midSelect"></select>
+            </td>
+        </tr>
+        <tr>
+            <td class="tt ct">商品編號</td>
+            <td class="pp"><?= $row['no']; ?></td>
+        </tr>
+        <tr>
+            <td class="tt ct">商品名稱</td>
+            <td class="pp"><input type="text" name="name" value="<?= $row['name']; ?>"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">商品價格</td>
+            <td class="pp"><input type="text" name="price" value="<?= $row['price']; ?>"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">規格</td>
+            <td class="pp"><input type="text" name="spec" value="<?= $row['spec']; ?>"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">庫存量</td>
+            <td class="pp"><input type="text" name="stock" value="<?= $row['stock']; ?>"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">商品圖片</td>
+            <td class="pp"><input type="file" name="img" id="img"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">商品介紹</td>
+            <td class="pp"><textarea name="intro" id="intro"><?= $row['intro']; ?></textarea></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="hidden" name="id" value="<?= $row['id']; ?>">
+        <input type="submit" value="修改">
+        <input type="reset" value="重置">
+        <input type="button" value="返回" onclick="location.href='?do=th'">
+    </div>
+</form>
 
 <script>
     //getTypes的第三個參數為陣列,分別是大分類和中分類的值
